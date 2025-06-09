f0acb3b2dbbcdebac76e61761562e000ae1cbcb4
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2025-06-09 15:33:42 -0700

    Update and rename test1.ino to temp1.ino

--- src/microcontroller/temp1.ino
+++ src/microcontroller/temp1.ino
@@ -1,37 +1,32 @@
-// SPDX-FileCopyrightText: 2020 Carter Nelson for Adafruit Industries
-//
-// SPDX-License-Identifier: MIT
-//
-#include <Adafruit_APDS9960.h>
-#include <Adafruit_BMP280.h>
-#include <Adafruit_LIS3MDL.h>
 #include <Adafruit_LSM6DS33.h>
 #include <Adafruit_LSM6DS3TRC.h>
-#include <Adafruit_SHT31.h>
-#include <Adafruit_Sensor.h>
-#include <PDM.h>
+#include <Adafruit_NeoPixel.h>
 
-Adafruit_APDS9960 apds9960; // proximity, light, color, gesture
-Adafruit_BMP280 bmp280;     // temperautre, barometric pressure
-Adafruit_LIS3MDL lis3mdl;   // magnetometer
+
 Adafruit_LSM6DS3TRC lsm6ds3trc; // accelerometer, gyroscope
 Adafruit_LSM6DS33 lsm6ds33;
-Adafruit_SHT31 sht30;       // humidity
 
-uint8_t proximity;
-uint16_t r, g, b, c;
-float temperature, pressure, altitude;
-float magnetic_x, magnetic_y, magnetic_z;
+#define LED_PIN     8
+
+
+#define LED_COUNT  1
+
+
+#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+
+
 float accel_x, accel_y, accel_z;
 float gyro_x, gyro_y, gyro_z;
-float humidity;
-int32_t mic;
+
+
 long int accel_array[6];
 long int check_array[6]={0.00, 0.00, 0.00, 0.00, 0.00, 0.00};
 
-extern PDMClass PDM;
-short sampleBuffer[256];  // buffer to read samples into, each sample is 16-bits
-volatile int samplesRead; // number of samples read
+
 
 bool new_rev = true;
 
@@ -42,7 +37,7 @@ void setup(void) {
 
   // initialize the sensors
 
-  lis3mdl.begin_I2C();
+
   lsm6ds33.begin_I2C();
   // check for readings from LSM6DS33
   sensors_event_t accel;
@@ -55,12 +50,15 @@ void setup(void) {
   accel_array[3] = gyro.gyro.x;
   accel_array[4] = gyro.gyro.y;
   accel_array[5] = gyro.gyro.z;
-  // if all readings are empty, then new rev
+
   for (int i =0; i < 5; i++) {
     if (accel_array[i] != check_array[i]) {
       new_rev = false;
       break;
     }
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(BRIGHTNESS);
   }
   // and we need to instantiate the LSM6DS3TRC
   if (new_rev) {
@@ -84,7 +82,9 @@ void loop(void) {
   accel_x = accel.acceleration.x;
   accel_y = accel.acceleration.y;
   accel_z = accel.acceleration.z;
-
+  strip.setPixelColor(0,strip.Color(abs(accel_x),   abs(accel_y),   abs(accel_z)));
+  strip.setBrightness(abs(accel_x)*abs(accel_y)*abs(accel_z)+25);
+  strip.show();
   
   Serial.println("\nFeather Sense Sensor Demo");
   Serial.println("---------------------------------------------");
